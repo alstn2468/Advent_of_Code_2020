50dc782e4df6da5f5e32acda1ba5d6b7f6720a02
alstn2468 <alstn2468_@naver.com>
2020-12-12 20:00:53 +0900

    Add day 1 python part 2 solution

--- Python/Day1/code.py
+++ Python/Day1/code.py
@@ -33,6 +33,18 @@ For example, suppose your expense report contained the following:
 
 In this list, the two entries that sum to 2020 are 1721 and 299.
 Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers
+you a starfish coin they had left over from a past vacation. They offer you
+a second one if you can find three numbers in your expense report that meet
+the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979,
+366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum
+to 2020?
 """
 
 from functools import reduce
@@ -46,17 +58,32 @@ def part1(numbers):
     )
 
 
+def part2(numbers):
+    return reduce(
+        lambda x, y: x * y,
+        list(*filter(lambda comb: sum(comb) == 2020, combinations(numbers, 3))),
+    )
+
+
 numbers = None
 
 with open("./input.txt", "r") as f:
     numbers = list(map(lambda line: int(line.rstrip("\n")), f.readlines()))
 
 with open("./output.txt", "w") as f:
-    f.write(f"part1 solution : {part1(numbers)}")
+    f.write(f"part1 solution : {part1(numbers)}\n")
+    f.write(f"part2 solution : {part2(numbers)}\n")
 
-TEST_CASE = {
+PART_ONE_TEST_CASE = {
     "numbers": [1721, 979, 366, 299, 675, 1456],
     "result": 514579,
 }
 
-print(part1(TEST_CASE["numbers"]) == TEST_CASE["result"])
+PART_TWO_TEST_CASE = {
+    "numbers": [1721, 979, 366, 299, 675, 1456],
+    "result": 241861950,
+}
+
+print(f'part1 test result : {part1(PART_ONE_TEST_CASE["numbers"]) == PART_ONE_TEST_CASE["result"]}')
+print(f'part2 test result : {part2(PART_TWO_TEST_CASE["numbers"]) == PART_TWO_TEST_CASE["result"]}')
+
