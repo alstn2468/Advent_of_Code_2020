cc2bb68abfa28c8b7c3393b4d22133cb3a216143
alstn2468 <alstn2468_@naver.com>
2020-12-13 18:10:33 +0900

    Add day 3 python part 2 solution

--- Python/Day3/code.py
+++ Python/Day3/code.py
@@ -68,24 +68,62 @@ encounter 7 trees.
 
 Starting at the top-left corner of your map and following a slope of right
 3 and down 1, how many trees would you encounter?
+
+--- Part Two ---
+Time to check the rest of the slopes - you need to minimize the probability
+of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the
+following slopes, you start at the top-left corner and traverse the map all
+the way to the bottom:
+
+- Right 1, down 1.
+- Right 3, down 1. (This is the slope you already checked.)
+- Right 5, down 1.
+- Right 7, down 1.
+- Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s)
+respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on
+each of the listed slopes?
 """
 
-grid = None
-TREE = "#"
+from functools import reduce
 
 
-def part1(grid):
+def get_tree_count(grid, row_move, col_move):
+    TREE = "#"
     position, result = [0, 0], 0
     col_length, row_length = len(grid), len(grid[0])
     while position[0] + 1 <= col_length:
         if grid[position[0]][position[1] % row_length] == TREE:
             result += 1
-        position = [position[0] + 1, position[1] + 3]
+        position = [position[0] + col_move, position[1] + row_move]
     return result
+
+
+def part1(grid):
+    return get_tree_count(grid, 3, 1)
+
+
+def part2(grid):
+    slopes = [
+        [1, 1],
+        [1, 3],
+        [1, 5],
+        [1, 7],
+        [2, 1],
+    ]
+    return reduce(
+        lambda x, y: x * y,
+        [get_tree_count(grid, slope[1], slope[0]) for slope in slopes],
+    )
 
 
 with open("./input.txt", "r") as input_file, open("./output.txt", "w") as output_file:
     grid = list(map(lambda line: line.rstrip("\n"), input_file.readlines()))
 
-    output_file.write(f"part1 solution : {part1(grid)}")
-
+    output_file.write(f"part1 solution : {part1(grid)}\n")
+    output_file.write(f"part2 solution : {part2(grid)}\n")
